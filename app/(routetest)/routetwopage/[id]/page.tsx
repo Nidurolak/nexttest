1a8f2b5d25a75363eac0db6af434561bf9bf133f
Nidurolak <jaewoon08@gmail.com>
2025-04-18 13:07:18 +0900

    feat : 다이내믹 라우팅 테스트 !@#250218-check

--- app/(routetest)/routetwopage/[id]/page.tsx
+++ app/(routetest)/routetwopage/[id]/page.tsx
@@ -1,5 +1,17 @@
-export default function DynamicTest() {
+import { use } from 'react'
+type Props = {
+    params: Promise<{ id: string }>;
+};
 
+
+export default async function DynamicTest(props: Props) {
+
+    //서버단에서 정보를 불러오는 것, use client를 써야 클라단에서 활용 가능
+    const { id } = await props.params;
+    console.group("그룹")
+    console.log('params:', (await props.params).id);
+    console.groupEnd()
+    console.log('params:', (id));
     return (<h1>이건 다이네믹 테스트입니다.</h1>)
 }
 
---
+++ app/(routetest)/routetwopage/page.tsx
@@ -0,0 +1,6 @@
+export default function DynamicShortTest() {
+
+    return (<h1>이건 다이네믹 테스트입니다123.</h1>)
+}
+
+//잔디심기
