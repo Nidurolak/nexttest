feff5153f96a2a09a702dcdb41645837ccb5d152
Nidurolak <jaewoon08@gmail.com>
2025-04-20 21:48:40 +0900

    feat : 레이아웃 위치 변경 테스트

--- app/(routetest)/layout.tsx
+++ app/(routetest)/layout.tsx
@@ -1,8 +1,9 @@
 //레이아웃이 필요한 이유 - 헤더같은 것들 붙이기 좋으니깐?
 //기본 페이지 말고, 분할된 페이지에도 레이아웃을 붙일 수 있다
 
+import { Metadata } from 'next';
 //메타데이터는 레이아웃과 비슷해보이지만 병합된다는 차이점이 있다.
-export const metadata = {
+export const metadata: Metadata = {
     title: 'jaewoo | route!',
     description: '이건  라우트디스크립션이야',
 }
