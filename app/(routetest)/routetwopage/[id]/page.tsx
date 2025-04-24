05e1570d31393ea42f8b42056c81113c1b6c8075
Nidurolak <jaewoon08@gmail.com>
2025-04-25 00:19:38 +0900

    feat : 다이내믹 라우터 서버단 적용 버전 연습

--- app/(routetest)/routetwopage/[id]/page.tsx
+++ app/(routetest)/routetwopage/[id]/page.tsx
@@ -1,18 +1,32 @@
-import { use } from 'react'
 type Props = {
-    params: Promise<{ id: string }>;
+    params: { id: string };
+    searchParams: { [key: string]: string | string[] | undefined };
 };
 
+export default async function DynamicTest({ params, searchParams }) {
 
-export default async function DynamicTest(props: Props) {
+    //서버단에서 사용할 거면 이렇게 구조분해를 하고 에이싱크를 붙이고 해야하나보다.
+    const { id } = await params;
+    //이렇게하면 서치파라미터의 region만 골라 가져온다.
+    //const { region } = await searchParams
+    //이렇게하면 서치파라미터의 region돠 ths만 골라 가져온다.
+    const { region, thx } = await searchParams
 
-    //서버단에서 정보를 불러오는 것, use client를 써야 클라단에서 활용 가능
-    const { id } = await props.params;
-    console.group("그룹")
-    console.log('params:', (await props.params).id);
-    console.groupEnd()
-    console.log('params:', (id));
-    return (<h1>이건 다이네믹 테스트입니다.</h1>)
+    console.group("그룹");
+    console.log('params:', id);
+    console.log('searchParams:', region);
+    console.log('searchParams2:', thx);
+    console.groupEnd();
+
+    return <h1>이건 다이네믹 테스트입니다.{id} {region}</h1>;
 }
 
-//잔디심기
+//그러면 클라단에선 어떻게 받을 까?
+/*
+"use client"
+
+
+
+
+
+*/
