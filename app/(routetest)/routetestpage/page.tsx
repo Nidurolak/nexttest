400025c1e7616af8f6a5b73c5b0673a00f625a40
Nidurolak <jaewoon08@gmail.com>
2025-05-02 23:57:38 +0900

    feat : 리액트 쿼리 추가

--- app/(routetest)/routetestpage/page.tsx
+++ app/(routetest)/routetestpage/page.tsx
@@ -1,4 +1,19 @@
-export default function RouteOnepage() {
+"use client"
 
-    return <h1>라우터 원 페이지</h1>
+import { useState } from "react"
+
+
+
+export default function RouteOnepage() {
+    const baseurl = process.env.NEXT_PUBLIC_BASE_URL;
+    const [movies, setMovies] = useState();
+    const getMovies = async () => {
+        const response = await fetch(
+            baseurl
+        )
+    }
+
+
+    return <><h1>라우터 원 페이지</h1>
+        { }</>
 }
