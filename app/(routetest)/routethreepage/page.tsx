e2f6e91ced006733e1023b099b64fdec180580ad
Nidurolak <jaewoon08@gmail.com>
2025-05-20 23:16:09 +0900

    feat : 서버사이드 컴포넌트 구현

--- app/(routetest)/routethreepage/page.tsx
+++ app/(routetest)/routethreepage/page.tsx
@@ -1,4 +1,21 @@
 
-export default async function SeverTest() {
-    return <h1>이건 다이네믹 테스트입니다.</h1>;
+export const metadata = {
+    title: "서버컴포넌트 테스트",
+}
+
+const URL = process.env.NEXT_PUBLIC_BASE_URL + "/movies"
+
+async function getMovies() {
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+
+export default async function ServerTest() {
+    console.log(URL)
+    const movies = await getMovies();
+    return <>
+        <h1>이건 서버 컴포넌트 테스트입니다.</h1>
+        <div>{JSON.stringify(movies)}</div>
+    </>;
 }
